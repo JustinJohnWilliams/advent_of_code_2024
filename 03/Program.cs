22f6145fbad9cd22bc95ae23cfc33e35a15274eb
Justin Williams <justinjohnwilliams@gmail.com>
2024-12-03 07:00:49 -0600

    Day 3: Part 2

--- 03/Program.cs
+++ 03/Program.cs
@@ -50,6 +50,51 @@ Console.WriteLine($"*************Day 3  DONE*************");
 
     var result = 0;
 
+    string input = File.ReadAllText(file);
+
+    string mulPattern = @"mul\((\d{1,3}),(\d{1,3})\)";
+    string controlPattern = @"\b(do|don't)\(\)";
+
+    var results = new List<(int, int)>();
+
+    bool doState = true;
+
+    int position = 0;
+    while (position < input.Length)
+    {
+        var controlMatch = Regex.Match(input.Substring(position), controlPattern);
+        if (controlMatch.Success && controlMatch.Index == 0)
+        {
+            doState = controlMatch.Value == "do()";
+            position += controlMatch.Length;
+            continue;
+        }
+
+        var mulMatch = Regex.Match(input.Substring(position), mulPattern);
+        if (mulMatch.Success && mulMatch.Index == 0)
+        {
+            if (doState)
+            {
+                int x = int.Parse(mulMatch.Groups[1].Value);
+                int y = int.Parse(mulMatch.Groups[2].Value);
+
+                results.Add((x, y));
+                result += x * y;
+            }
+            position += mulMatch.Length;
+            continue;
+        }
+
+        position++;
+    }
+
+    /*
+    foreach (var tuple in results)
+    {
+        Console.WriteLine($"({tuple.Item1}, {tuple.Item2})");
+    }
+    */
+
     sw.Stop();
 
     return(result, sw.Elapsed.TotalMilliseconds);
