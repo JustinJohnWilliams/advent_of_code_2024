65f841dad0cd9e5caac7547c12072827327105fb
Justin Williams <justinjohnwilliams@gmail.com>
2024-12-05 13:35:14 -0600

    Day 5: Part 2 wip

--- 05/Program.cs
+++ 05/Program.cs
@@ -63,7 +63,95 @@ Console.WriteLine($"*************Day 5  DONE*************");
     sw.Start();
 
     var result = 0;
+    var contents = File.ReadAllLines(file);
+
+    var rules = contents.Where(c => c.Contains('|')).ToList()
+                    .Select(c => c.Split('|'))
+                    .Select(c => new KeyValuePair<string, string>(c[0], c[1]))
+                    .ToList();
+
+    var updates = contents.Where(c => c.Contains(',')).ToList();
+
+    var badUpdates = new List<string>();
+    foreach(var update in updates)
+    {
+        var updateParts = update.Split(',');
+        for(int i = 0; i < updateParts.Length - 1; i++)
+        {
+            var page = updateParts[i].ToString();
+            var nextPage = updateParts[i+1].ToString();
+            if(!rules.Any(c => c.Key == page) || !rules.Any(c => c.Key == page && c.Value == nextPage))
+            {
+                badUpdates.Add(update);
+                break;
+            }
+        }
+    }
+
+    var goodUpdates = new List<string>();
+    //oh boi, this is gonna be ugly
+    var permutations = new List<string>();
+    foreach(var update in badUpdates)
+    {
+        permutations.Clear();
+        permutations.AddRange(GetPermutations(update.Split(',')));
+        foreach(var permutation in permutations)
+        {
+            Console.WriteLine(permutation);
+            var updateParts = permutation.Split(',');
+            for(int i = 0; i < updateParts.Length - 1; i++)
+            {
+                var page = updateParts[i].ToString();
+                var nextPage = updateParts[i+1].ToString();
+                if(!rules.Any(c => c.Key == page) || !rules.Any(c => c.Key == page && c.Value == nextPage))
+                {
+                    break;
+                }
+
+                if(i + 1 == updateParts.Length - 1)
+                {
+                    goodUpdates.Add(permutation);
+                }
+            }
+        }
+    }
+
+
+    //goodUpdates.ForEach(Console.WriteLine);
+
+    result += goodUpdates.Select(c => c.Split(',')
+                         .Select(int.Parse)
+                         .ToList())
+                .Select(pages => pages[pages.Count / 2]).ToList()
+                .Sum();
+
     sw.Stop();
 
     return (result, sw.Elapsed.TotalMilliseconds);
+}
+
+List<string> GetPermutations(string[] numbers)
+{
+    var result = new List<string>();
+
+    if (numbers.Length == 1)
+    {
+        result.Add(numbers[0]);
+        return result;
+    }
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        var remaining = new List<string>(numbers);
+        remaining.RemoveAt(i);
+
+        var subPermutations = GetPermutations(remaining.ToArray());
+
+        foreach (var subPermutation in subPermutations)
+        {
+            result.Add(numbers[i] + "," + subPermutation);
+        }
+    }
+
+    return result;
 }
