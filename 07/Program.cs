6aac34f18ee8fcdcd8106505fdcf6539d71b1a03
Justin Williams <justinjohnwilliams@gmail.com>
2024-12-06 16:56:42 -0600

    Day 7: Part 1

---
+++ 07/Program.cs
@@ -0,0 +1,33 @@
+Console.WriteLine();
+Console.WriteLine($"*************Day 7 START*************");
+
+var p1 = part_one("input.txt");
+var p2 = part_two("input.txt");
+
+Console.WriteLine($"Part 1 Result: {p1.result} \t: {p1.ms}ms");
+Console.WriteLine($"Part 2 Result: {p2.result} \t: {p2.ms}ms");
+Console.WriteLine($"*************Day 7  DONE*************");
+
+(long result, double ms) part_one(string file)
+{
+    var sw = new System.Diagnostics.Stopwatch();
+    sw.Start();
+
+    var result = 0;
+
+    sw.Stop();
+
+    return (result, sw.Elapsed.TotalMilliseconds);
+}
+
+(long result, double ms) part_two(string file)
+{
+    var sw = new System.Diagnostics.Stopwatch();
+    sw.Start();
+
+    var result = 0;
+
+    sw.Stop();
+
+    return (result, sw.Elapsed.TotalMilliseconds);
+}
