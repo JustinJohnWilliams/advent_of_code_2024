2d1718239e587a67057f984b599980495709b0a5
Justin Williams <justinjohnwilliams@gmail.com>
2024-12-11 09:37:23 -0600

    Day 11: Part 2

--- 11/Program.cs
+++ 11/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine($"*************Day 11 START*************");
 var p1 = part_one("input.txt");
 var p2 = part_two("input.txt");
 
-Console.WriteLine($"Part 1 Result: {p1.result} \t: {p1.ms}ms");
+Console.WriteLine($"Part 1 Result: {p1.result} \t\t: {p1.ms}ms");
 Console.WriteLine($"Part 2 Result: {p2.result} \t: {p2.ms}ms");
 Console.WriteLine($"*************Day 11  DONE*************");
 
@@ -37,10 +37,13 @@ Console.WriteLine($"*************Day 11  DONE*************");
     var sw = new System.Diagnostics.Stopwatch();
     sw.Start();
 
-    var result = 0;
+    long result = 0;
 
-    var input = File.ReadAllText(file);
+    var input = File.ReadAllText(file).Trim();
+    var stones = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToList();
+    int blinks = 75;
 
+    result += CountStones(stones, blinks);
 
     sw.Stop();
 
@@ -73,4 +76,59 @@ List<long> Blink(List<long> stones)
     }
 
     return newStones;
+}
+
+long CountStones(IEnumerable<long> stones, int blinks)
+{
+    var cache = new Dictionary<long, long>();
+
+    cache.SafeAdd(stones);
+
+    for (int i = 0; i < blinks; i++)
+    {
+        foreach (var (stone, count) in cache.ToList())
+        {
+            cache.SafeDecrement(stone, count);
+
+            if (stone == 0)
+            {
+                cache.SafeAdd(1, count);
+            }
+            else if (stone.ToString().Length % 2 == 0)
+            {
+                string s = stone.ToString();
+                int mid = s.Length / 2;
+                long left = long.Parse(s[..mid]);
+                long right = long.Parse(s[mid..]);
+                cache.SafeAdd(left, count);
+                cache.SafeAdd(right, count);
+            }
+            else
+            {
+                cache.SafeAdd(stone * 2024, count);
+            }
+        }
+    }
+
+    return cache.Values.Sum(c => c);
+}
+
+public static class Extensions
+{
+    public static void SafeAdd(this Dictionary<long, long> dictionary, IEnumerable<long> stones)
+    {
+        stones.ToList().ForEach(c => dictionary.SafeAdd(c, 1));
+    }
+    public static void SafeAdd(this Dictionary<long, long> dictionary, long stone, long count)
+    {
+        if(!dictionary.ContainsKey(stone)) dictionary[stone] = count;
+        else dictionary[stone] += count;
+    }
+
+    public static void SafeDecrement(this Dictionary<long, long> dictionary, long stone, long count)
+    {
+        dictionary[stone] -= count;
+
+        if (dictionary[stone] == 0) dictionary.Remove(stone);
+    }
 }
