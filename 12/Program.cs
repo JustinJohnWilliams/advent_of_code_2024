5e7e795bcfc657f245ee66aa658a75d26595c6b8
Justin Williams <justinjohnwilliams@gmail.com>
2024-12-12 19:13:15 -0600

    Day 12: Part 2

--- 12/Program.cs
+++ 12/Program.cs
@@ -29,7 +29,7 @@ Console.WriteLine($"*************Day 12  DONE*************");
         {
             if (!visited.Contains((x, y)))
             {
-                var (area, perimeter, plant) = TraverseGarden(garden, x, y, visited);
+                var (area, perimeter, plant, _) = TraverseGarden(garden, x, y, visited);
                 //Console.WriteLine($"{plant} - area: {area}, perimeter: {perimeter}");
                 result += area * perimeter;
             }
@@ -41,7 +41,39 @@ Console.WriteLine($"*************Day 12  DONE*************");
     return (result, sw.Elapsed.TotalMilliseconds);
 }
 
-(long area, long perimeter, char plant) TraverseGarden(char[][] grid, int startX, int startY, HashSet<(int x, int y)> visited)
+(long result, double ms) part_two(string file)
+{
+    var sw = new System.Diagnostics.Stopwatch();
+    sw.Start();
+
+    long result = 0;
+
+    var garden = File.ReadAllLines(file)
+                     .Select(line => line.Select(c => c).ToArray())
+                     .ToArray();
+    var rows = garden.Length;
+    var cols = garden[0].Length;
+    var visited = new HashSet<(int x, int y)>();
+
+    for (var x = 0; x < rows; x++)
+    {
+        for (var y = 0; y < cols; y++)
+        {
+            if (!visited.Contains((x, y)))
+            {
+                var (_, _, _, cluster) = TraverseGarden(garden, x, y, visited);
+                var cost = LukeIsASmartyPants(cluster);
+                result += cost;
+            }
+        }
+    }
+
+    sw.Stop();
+
+    return (result, sw.Elapsed.TotalMilliseconds);
+}
+
+(long area, long perimeter, char plant, List<(int x, int y)> cluster) TraverseGarden(char[][] grid, int startX, int startY, HashSet<(int x, int y)> visited)
 {
     var rows = grid.Length;
     var cols = grid[0].Length;
@@ -49,6 +81,7 @@ Console.WriteLine($"*************Day 12  DONE*************");
     var plant = grid[startX][startY];
     long area = 0;
     long perimeter = 0;
+    var cluster = new List<(int x, int y)>();
 
     queue.Enqueue((startX, startY));
     visited.Add((startX, startY));
@@ -56,6 +89,7 @@ Console.WriteLine($"*************Day 12  DONE*************");
     while (queue.Any())
     {
         var (x, y) = queue.Dequeue();
+        cluster.Add((x, y));
         area++;
 
         foreach (var (nx, ny) in GetNeighbors(x, y, rows, cols))
@@ -72,7 +106,7 @@ Console.WriteLine($"*************Day 12  DONE*************");
         }
     }
 
-    return (area, perimeter, plant);
+    return (area, perimeter, plant, cluster);
 }
 
 IEnumerable<(int x, int y)> GetNeighbors(int x, int y, int rows, int cols)
@@ -83,19 +117,44 @@ IEnumerable<(int x, int y)> GetNeighbors(int x, int y, int rows, int cols)
     }
 }
 
-(long result, double ms) part_two(string file)
+long LukeIsASmartyPants(List<(int x, int y)> region)
 {
-    var sw = new System.Diagnostics.Stopwatch();
-    sw.Start();
+    var area = region.Count;
+    var vertices = 0;
 
-    long result = 0;
+    foreach (var (x, y) in region)
+    {
+        var top = region.Contains((x - 1, y));
+        var bottom = region.Contains((x + 1, y));
+        var left = region.Contains((x, y - 1));
+        var right = region.Contains((x, y + 1));
 
-    var garden = File.ReadAllLines(file)
-                     .Select(line => line.Select(c => c).ToArray())
-                     .ToArray();
+        var topLeft = region.Contains((x - 1, y - 1));
+        var topRight = region.Contains((x - 1, y + 1));
+        var bottomLeft = region.Contains((x + 1, y - 1));
+        var bottomRight = region.Contains((x + 1, y + 1));
 
-    sw.Stop();
+        if (!top && !left)
+            vertices++;
+        if (top && left && !topLeft)
+            vertices++;
 
-    return (result, sw.Elapsed.TotalMilliseconds);
+        if (!top && !right)
+            vertices++;
+        if (top && right && !topRight)
+            vertices++;
+
+        if (!bottom && !left)
+            vertices++;
+        if (bottom && left && !bottomLeft)
+            vertices++;
+
+        if (!bottom && !right)
+            vertices++;
+        if (bottom && right && !bottomRight)
+            vertices++;
+    }
+
+    //Console.WriteLine($"Area: {area}, Vertices: {vertices}");
+    return area * vertices;
 }
-
